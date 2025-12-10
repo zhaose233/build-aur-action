3bd6a2542d422c7b1c4bf00cee3de1c35f465869
Zhaose <70591705+zhaose233@users.noreply.github.com>
2025-12-10 10:38:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-git clone --branch "$1" --single-branch https://github.com/archlinux/aur.git "$1"
+ZHAOSE_PKGS_TMP="zhaose-pkgbuilds-tmp"
+git clone https://github.com/zhaose233/zhaose-pkgbuilds.git "$ZHAOSE_PKGS_TMP"
+if [ -d "$ZHAOSE_PKGS_TMP/$1" ]; then
+  mkdir -p "$1"
+  mv "$ZHAOSE_PKGS_TMP/$1"/* "$1/"
+else
+  git clone --branch "$1" --single-branch https://github.com/archlinux/aur.git "$1"
+fi
+
 cd "$1"
 makepkg -sf --noconfirm --skippgpcheck --nocheck
